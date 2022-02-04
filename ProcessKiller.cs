ef4d4ffc5b69d86977bdb196e349119ada6cf902
Yaroslav Shevchenko <schev4enko3@gmail.com>
2022-02-04 08:47:42 +0300

    Add project files.

---
+++ ProcessKiller.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Process_killer
+{
+    internal class ProcessKiller
+    {
+        private string Name { get; }
+
+        private uint LifeTime { get; }
+
+        private uint CheckFrequency { get; }
+
+        private DateTime LifeTimeEnd { get; set; }
+
+        public ProcessKiller(string processName, uint lifeTime, uint сheckFrequency)
+        {
+            Name = processName;
+            LifeTime = lifeTime;
+            CheckFrequency = сheckFrequency;
+            StartCheckTimer();
+        }
+
+        public void StartCheckTimer()
+        {
+            Timer checkProcessTimer = new Timer(CheckProcess, null, 0, CheckFrequency * 60000);
+            Console.ReadLine();
+        }
+
+        private void CheckProcess(Object obj)
+        {
+            try
+            {
+                Process[] process = Process.GetProcessesByName(Name);
+                Console.WriteLine($"Spy the process: { process[0].ProcessName}");
+
+                if (LifeTimeEnd.Equals(DateTime.MinValue))
+                {
+                    LifeTimeEnd = DateTime.Now;
+                    LifeTimeEnd = LifeTimeEnd.AddMinutes(LifeTime);
+                }
+                else if (LifeTimeEnd.TimeOfDay <= DateTime.Now.TimeOfDay)
+                {
+                    foreach (var item in process)
+                        item.Kill();
+                    Console.WriteLine($"Process was killed at {DateTime.Now.TimeOfDay}");
+                    Environment.Exit(0);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Process not found!");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Process_killer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Run with three parameters:\n1. Name of the process.\n2. Timer until the end of the process life.\n3. Periodicity of checking the life of the process.");
+                Environment.Exit(0);
+            }
+            string processName = args[0];
+            uint lifeTime = Convert.ToUInt32(args[1]);
+            uint сheckFrequency = Convert.ToUInt32(args[2]);
+
+            ProcessKiller processKiller = new ProcessKiller(processName, lifeTime, сheckFrequency);
+        }
+    }
+}
